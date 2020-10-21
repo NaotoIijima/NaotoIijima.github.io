07a71ce934218bedc0e2916c6ab62e4bd84fc090
NaotoIijima <50772589+NaotoIijima@users.noreply.github.com>
2020-10-21 11:43:39 +0900

    Add files via upload

---
+++ configs/resources/audio/cocatemale.m
@@ -0,0 +1,22 @@
+clear
+
+%%
+abspath = pwd;
+dirname1 = [abspath,'\signal_45.wav'];
+dirname2 = [abspath,'\signal_90.wav'];
+dirname3 = [abspath,'\signal_135.wav'];
+dirname4 = [abspath,'\signal_-45.wav'];
+dirname5 = [abspath,'\signal_-90.wav'];
+dirname6 = [abspath,'\signal_-135.wav'];
+%%
+[s1,~]=audioread(dirname1);
+[s2,~]=audioread(dirname2);
+[s3,~]=audioread(dirname3);
+[s4,~]=audioread(dirname4);
+[s5,~]=audioread(dirname5);
+[s6,~]=audioread(dirname6);
+all_sig_left=[s1(1:239616,:);s2(1:239616,:);s3(1:239616,:)];
+all_sig_right=[s4(1:239616,:);s5(1:239616,:);s6(1:239616,:)];
+%%
+audiowrite('signal_all_left.wav',all_sig_left,48000);
+audiowrite('signal_all_right.wav',all_sig_right,48000);
